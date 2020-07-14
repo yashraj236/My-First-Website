ba192b218edb2be988521a99cd0bdc387a35ef07
Yashraj Agarwal <50520646+yashraj236@users.noreply.github.com>
2020-07-14 16:56:22 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,78 @@
+const {
+    Order,
+    ProductCart
+} = require("../models/order")
+
+
+exports.getOrderById = (req, res, next, id) => {
+    Order.findById(id)
+        .populate("products.product", "name price")
+        .exec((err, order) => {
+            if (err) {
+                res.status(400).json({
+                    error: "No Order found in DB"
+                })
+            }
+            req.order = order
+            next()
+        })
+
+}
+
+
+exports.createOrder = (req, res) => {
+    req.body.order.user = req.profile;
+    const order = new Order(req.body.order)
+    order.save((err, order) => {
+        if (err) {
+            res.status(400).json({
+                error: "Failed to save your Order in DB"
+            })
+        }
+        res.json(order)
+    })
+}
+
+
+exports.getAllOrders = (req, res) => {
+    Order.find()
+        .populate("user", "_id name ")
+        .exec((err, order) => {
+            if (err) {
+                res.status(400).json({
+                    error: "No Orders found in DB"
+                })
+            }
+            res.json(order)
+        })
+}
+
+exports.getOrderStatus = (req, res) => {
+
+    res.json(Order.schema.status.path("status").enumValues)
+
+};
+
+
+exports.updateStatus = (req, res) => {
+
+    Order.update({
+            _id: req.body.orderId
+        }, {
+            $set: {
+                status: req.body.status
+            }
+        },
+        (err, order) => {
+            if (err) {
+                res.status(400).json({
+                    error: "Cannot Update Order Status"
+                })
+            }
+            res.json(order)
+        }
+
+
+    )
+
+};
